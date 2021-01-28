07ab1582f7752d9a22c990836e1a86ace0c86eff
PedroChavess <pedrochaves30@gmail.com>
2021-01-28 20:52:46 -0300

    codigo ao final da terceira aula da imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,71 @@
+import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-
-export const QuizContainer = styled.div`
-    width: 100%;
-    max-width: 350px;
-    padding-top: 45px;
-    margin: auto 10%;
-    @media screen and (max-width: 500px) {
-      margin: auto;
-      padding: 15px; 
-    }
-
-    h1, h2, h3 {
-      font-size: 16px;
-      font-weight: 700;
-      line-height: 1;
-      margin-bottom:0;
-    }
-    p{
-      font-size: 14px;
-      font-weight: 400;
-      line-height:1;
-    }
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno de useState', name, setName);
+
   return (
-    <QuizBackground backgroundImage = {db.bg}>
-      <QuizContainer>https://cinesiageek.com.br/wp-content/uploads/2017/06/Harry-Potter-Wallpapers-Archives-Page-2-of-4-Wallpaper.jpg
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz Wizarding World</title>
+        <link rel="sortcut icon" href="https://i.dlpng.com/static/png/6615198_preview.png" />;
+        {/* colocar meta tag */}
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+
         <Widget>
           <Widget.Header>
             <h1> Harry Potter</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Draco Dormiens Nunquam Titillandus </p>
+            <form onSubmit={function (insfosDoEvento) {
+              insfosDoEvento.preventDefault(); /* Faz com que tire o comportamento padrao do form, faz com que não fique atualizando a página
+              sempre que clicar no botão de jogar */
+
+              router.push(`/quiz?name=${name}`);
+
+              console.log('Fazendo uma submissão por meio do react');
+              /* router manda para próxima página */
+            }}
+
+
+            > {/* onSubmit => para escrever em react */}
+              <Input
+                name="nomeDoUsuario"
+                onChange={(insfosDoEvento) => setName(insfosDoEvento.target.value)}
+                placeholder="Insira aqui seu nome"
+                value={name}
+              />
+
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -62,7 +89,7 @@ function Title(props) { //propcidades/propriedades <
     {props.children}
     </h1>
     )
-  } 
+  }
 
 const BackgroundImage = styled.div`
 background-image: url(${db.bg});
